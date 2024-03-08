4a8d40665a71c63c5e85f75ef97347cb6aebddbf
Lisa Sjögren <lisa.m.sjogren@gmail.com>
2024-03-08 17:07:20 +0100

    accept post with file

--- bandit/api/app.py
+++ bandit/api/app.py
@@ -1,11 +1,13 @@
-from ml_logic.preprocessor import preprocess_data
-from ml_logic.postprocessor import postprocess_prediction
-from ml_logic.encoder import encode_data
-from ml_logic.model import load_model
+from bandit.ml_logic.preprocessor import preprocess_data
+from bandit.ml_logic.postprocessor import postprocess_prediction
+from bandit.ml_logic.encoder import encode_data
+from bandit.ml_logic.model import load_model
+from pretty_midi import PrettyMIDI
 
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 
 app = FastAPI()
+
 
 @app.get("/")
 def some_function():
@@ -14,15 +16,17 @@ def some_function():
 # Get prediction
 # Maybe this needs to be a post given the midi input data...
 #http://127.0.0.1:8000/predict?midi_file=%22cat%22
-@app.get("/predict")
-def predict(midi_file):
+@app.post("/predict/")
+async def predict(file: UploadFile):
+    ### Receiving and decoding the image
+    content = await file.read()
     # preprocess midi-file
     # predict with model
     # return prediction
     # postprocess midi-file to get a midi
-    return f"This is my prediction for {midi_file}! Tralala!"
+    return f"This is my prediction for {file.filename}! Tralala!"
 
-@app.get("/midi-to-wav")
+@app.post("/midi-to-wav")
 def midi_to_wav(midi_file):
     # if we need to transform the audio into wav to play it
     # we could do this with both input and output
