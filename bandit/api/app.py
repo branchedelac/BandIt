368d99c581c2274d24a57e7b65cf4680cead5052
Lisa Sjögren <lisa.m.sjogren@gmail.com>
2024-03-08 18:56:27 +0100

    call preprocessor with file as input

--- bandit/api/app.py
+++ bandit/api/app.py
@@ -2,9 +2,9 @@ from bandit.ml_logic.preprocessor import preprocess_data
 from bandit.ml_logic.postprocessor import postprocess_prediction
 from bandit.ml_logic.encoder import encode_data
 from bandit.ml_logic.model import load_model
-from pretty_midi import PrettyMIDI
-
-from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import JSONResponse
+from fastapi import FastAPI, UploadFile
+import io
 
 app = FastAPI()
 
@@ -13,18 +13,25 @@ app = FastAPI()
 def some_function():
     print("Hello!")
 
+
 # Get prediction
 # Maybe this needs to be a post given the midi input data...
-#http://127.0.0.1:8000/predict?midi_file=%22cat%22
+# http://127.0.0.1:8000/predict?midi_file=%22cat%22
 @app.post("/predict/")
 async def predict(file: UploadFile):
-    ### Receiving and decoding the image
-    content = await file.read()
-    # preprocess midi-file
-    # predict with model
-    # return prediction
-    # postprocess midi-file to get a midi
-    return f"This is my prediction for {file.filename}! Tralala!"
+    try:
+        ### Receiving and decoding the image
+        content = await file.read()
+        # Convert the bytes content into a file-like object
+        midi_file_stream = io.BytesIO(content)
+        # Do something with the parsed MIDI data here
+        preprocess_data([midi_file_stream], [file.filename])
+        return JSONResponse(content={"message": "MIDI file processed successfully"})
+
+    except ValueError as e:
+        # Return error response if something goes wrong
+        return JSONResponse(content={"error": str(e)}, status_code=500)
+
 
 @app.post("/midi-to-wav")
 def midi_to_wav(midi_file):
