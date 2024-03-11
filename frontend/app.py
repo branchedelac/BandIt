81c0fb5f871a9b922bf74b56ff89ba63b1ac5bcc
Lisa Sjögren <lisa.m.sjogren@gmail.com>
2024-03-11 21:57:28 +0100

    add progressive hotel california

--- frontend/app.py
+++ frontend/app.py
@@ -49,29 +49,18 @@ with st.form("my_form"):
     submitted = st.form_submit_button("Submit")
 
 
-    if submitted:
-        st.write("Processing your file....")
-        params = {}
+if submitted:
+    st.write("Processing your file... 🎶")
+    params = {}
+    # TODO call our prediction API
+    # Will the API return a midi file? Convert to wav?
+    #res = requests.get(url=base_url, params=params)
 
-        # TODO call our prediction API
-        # Will the API return a midi file?
-        #res = requests.get(url=base_url, params=params)
+    # Show the result!
+    st.write("Listen to your new drum arrangement!")
 
-        # Show the result!
-        st.write("Listen to your new drum track!")
-
-# TODO Make use of this audio player snippet
-# Do we need to convert the midi file to something else?
-audio_file = open(f'{os.getcwd()}/frontend/Vår.mp3', 'rb')
-audio_bytes = audio_file.read()
-st.audio(audio_bytes, format='mp3')
-
-# Not sure what the below does...
-sample_rate = 44100  # 44100 samples per second
-seconds = 2  # Note duration of 2 seconds
-frequency_la = 440  # Our played note will be 440 Hz
-# Generate array with seconds*sample_rate steps, ranging between 0 and seconds
-t = np.linspace(0, seconds, seconds * sample_rate, False)
-# Generate a 440 Hz sine wave
-note_la = np.sin(frequency_la * t * 2 * np.pi)
-#st.audio(note_la, sample_rate=sample_rate)
+    # TODO Make use of this audio player snippet
+    # Do we need to convert the midi file to something else?
+    audio_file = open(f'{os.getcwd()}/frontend/Hotel_California_1_pop2piano_drum_hack.wav', 'rb')
+    audio_bytes = audio_file.read()
+    st.audio(audio_bytes, format='mp3')
